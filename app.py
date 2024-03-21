78768a96feef75c18a06d68fc0100d0aa5bd6758
Joey Issa <joeyissa59@gmail.com>
2024-03-20 22:01:09 -0400

    done start base

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+@app.route('/search.html')
+def search():
+    return render_template('search.html')
+
+@app.route('/manual_search.html')
+def manual_search():
+    return render_template('manual_search.html')
+
 
 if __name__ == '__main__':
     app.run()
