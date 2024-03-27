db737757835ae0475ff6f3f860027f13a27c4b1d
joeyissa <joeyissa59@gmail.com>
2024-03-27 17:36:47 -0400

    done, database set up

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from urllib import request
 from flask import Flask, render_template, request, redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:issadogs@localhost/hotel'
+db = SQLAlchemy(app)
 
 # Dummy user data for demonstration
 users = {'user1': 'password1', 'user2': 'password2'}
 
 @app.route('/')
-def hello_world():  
+def hello_world():
     return render_template('index.html')
 
-@app.route('/search.html')
-def search():
-    return render_template('search.html')
+@app.route('/searchS.html')
+def search_page():
+    return render_template('searchS.html')
 
 @app.route('/manual_search.html')
 def manual_search():
@@ -25,7 +23,6 @@ def manual_search():
 @app.route('/book.html')
 def book():
     return render_template('book.html')
-
 
 @app.route('/gosearch.html')
 def gosearch():
@@ -59,50 +56,35 @@ def signin():
         # Authentication failed, redirect back to sign-in page
         return redirect(url_for('employee_signup'))
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
 
-#class Product(db.Model):
- #   id = db.Column(db.Integer, primary_key=True)
-  #  name = db.Column(db.String(100), nullable=False)
-   # description = db.Column(db.String(200))
-    #price = db.Column(db.Float)
+@app.route('/search', methods=['GET'])
+def search():
+    amenities = request.args.get('amenities')
+    price_high = request.args.get('pricehigh')
+    price_low = request.args.get('pricelow')
+    extendible = request.args.get('extendible')
 
-#@app.route('/search', methods=['GET'])
-#def search():
-#    amenities = request.args.get('amenities')
- #   pricehigh = request.args.get('pricehigh')
-  #  pricelow = request.args.get('pricelow')
-   # extendible = request.args.get('extendible')
+    # Start building the SQL query using parameterized query
+    query = Product.query.filter_by(isAvailable=True)
+    if amenities:
+        query = query.filter(Product.amenities.ilike(f'%{amenities}%'))
+    if price_high:
+        query = query.filter(Product.price <= float(price_high))
+    if price_low:
+        query = query.filter(Product.price >= float(price_low))
+    if extendible:
+        query = query.filter(Product.extendible == extendible)
 
-    # Start building the SQL query
-#    sql_query = "SELECT * FROM products WHERE 1=1"
-#
- #   if amenities:
-  #      sql_query += f" AND amenities ILIKE '%{amenities}%'"
-   # if pricehigh:
-    #    sql_query += f" AND price <= {pricehigh}"
-#    if pricelow:
- #       sql_query += f" AND price >= {pricelow}"
-  #  if extendible:
-   #     sql_query += f" AND extendible = '{extendible}'"
-    
-    #sql_query += f" AND isAvailable = 'TRUE'"
+    # Execute the query
+    results = query.all()
 
-    # Execute
-#    results = db.session.execute(text(sql_query))
-
-    # Format results
-#    html_results = ''
- #   for row in results:
-  #      html_results += f'<div class="room">'
-   #     html_results += f'<img src="{row.image}" alt="{row.name}">'
-    #    html_results += f'<div class="details">'
-     #   html_results += f'<h2>{row.name}</h2>'
-#        html_results += f'<p>Price: ${row.price} per night</p>'
- #       html_results += f'<button class="book-button" onclick="booked({row.RoomID})"><a href="book.html">Book</a></button>' #also make clicking this take the user to another page, keeping track of the specific room id they chose
-  #      html_results += f'</div>'
-   #     html_results += f'</div>'
-    
-    #return html_results
+    # Render template with search results
+    return render_template('search_results.html', results=results)
 
 if __name__ == '__main__':
     app.run()
